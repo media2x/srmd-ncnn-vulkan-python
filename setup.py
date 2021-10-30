e7cd5ba4767fb0ce802f7a53dc22ee0421042da6
ArchieMeng <archiemeng@protonmail.com>
2021-10-30 21:26:22 +0800

    Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+import pathlib
+import setuptools
+import sys
+
+# HACK: allows pip to load user site packages when running setup
+if "PYTHONNOUSERSITE" in os.environ:
+    os.environ.pop("PYTHONNOUSERSITE")
+
+sys.path.append(
+    str(
+        pathlib.Path.home()
+        / ".local/lib/python{}.{}/site-packages".format(
+            sys.version_info.major, sys.version_info.minor
+        )
+    )
+)
+sys.path.append(
+    "/usr/lib/python{}.{}/site-packages".format(
+        sys.version_info.major, sys.version_info.minor
+    )
+)
+
+import cmake_build_extension
+
+setuptools.setup(
+    ext_modules=[
+        cmake_build_extension.CMakeExtension(
+            name="srmd-ncnn-vulkan-python",
+            install_prefix="srmd_ncnn_vulkan_python",
+            write_top_level_init="from .srmd_ncnn_vulkan import Srmd",
+            source_dir=str(pathlib.Path(__file__).parent / "srmd_ncnn_vulkan_python"),
+            cmake_configure_options=[
+                "-DBUILD_SHARED_LIBS:BOOL=OFF",
+                "-DCALL_FROM_SETUP_PY:BOOL=ON",
+            ],
+        )
+    ],
+    cmdclass={"build_ext": cmake_build_extension.BuildExtension},
+)
